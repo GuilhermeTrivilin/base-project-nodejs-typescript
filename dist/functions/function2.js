26f571cbd6d8b8a36295669a9e5aa8a41e55019d
Guilherme Trivilin <trivilin.dev@hotmail.com>
2023-08-31 12:29:33 -0300

    removendo node modules e dist

--- dist/functions/function2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.function2 = void 0;
-const function2 = () => {
-    return `Executando a function 1`;
-};
-exports.function2 = function2;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const function1_1 = require("./functions/function1");
-const function2_1 = require("./functions/function2");
-const main = () => {
-    console.log((0, function1_1.function1)());
-    console.log((0, function2_1.function2)());
-};
-main();
